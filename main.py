0a2c21cb0041aea195ed2eec8f437165844bf174
tuckclub <tuckclub@gmail.com>
2021-11-02 07:05:55 +0700

    Display field worth_investing.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import numpy_financial as npf
 
 
 def format_row_data(data):
-    tint = "{:,.0f}"
-    tfloat = "{:,.2f}"
-    tpercent = "{:.0f}%"
-    templates = [tint, tint, tint, tint, tint, tint, tint, tpercent, tfloat, tpercent]
+    tint = '{:,.0f}'
+    tfloat = '{:,.2f}'
+    tpercent = '{:.0f}%'
+    tbool = '{}'
+    templates = [tint, tint, tint, tint, tint, tint, tint, tpercent, tfloat, tpercent, tbool]
     return [templates[idx].format(value) for idx, value in enumerate(data)]
 
 
@@ -29,12 +30,15 @@ col_widths = {
     'yrw': default_col_width,
     'npvw': default_col_width,
     'irrw': default_col_width,
+    'wthw': default_col_width,
 }
 display_width = sum(col_widths.values())
 thin_line = '-' * display_width
 thick_line = '=' * display_width
-row_template = '{:>{idw}}{:>{icw}}{:>{y1w}}{:>{y2w}}{:>{y3w}}{:>{y4w}}{:>{y5w}}{:>{yrw}}{:>{npvw}}{:>{irrw}}'
-headers = ['#', 'Initial Capital', 'Year 1', 'Year 2', 'Year 3', 'Year 4', 'Year 5', 'Yield Rate', 'NPV', 'IRR']
+row_template = '{:>{idw}}{:>{icw}}{:>{y1w}}{:>{y2w}}{:>{y3w}}{:>{y4w}}{:>{y5w}}' \
+               '{:>{yrw}}{:>{npvw}}{:>{irrw}}{:>{wthw}}'
+headers = ['#', 'Initial Capital', 'Year 1', 'Year 2', 'Year 3', 'Year 4', 'Year 5',
+           'Yield Rate', 'NPV', 'IRR', "Worth"]
 
 print(thick_line)
 print(row_template.format(*headers, **col_widths))
@@ -48,7 +52,8 @@ for line in stream_lines_from_file('Data_ลงทุน.txt', 'r'):
     npv = npf.npv(yield_percent / 100, cash_flows)
     irr = npf.irr(cash_flows)
     irr_percent = int(round(irr * 100))
-    row_data = raw_data + [npv, irr_percent]
+    worth_investing = npv > 0 and irr_percent > yield_percent
+    row_data = raw_data + [npv, irr_percent, worth_investing]
     formatted_row_data = format_row_data(row_data)
     print(row_template.format(*formatted_row_data, **col_widths))
 
